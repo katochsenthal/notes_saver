deac4e763b4aaf9e9ccdf619ed6c4ff051ebb5b1
katochsenthal <katoch.senthal@gmail.com>
2022-05-07 23:49:20 -0400

    adding post notes api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { get } = require("express/lib/response");
 
 const PORT = 3001;
 
@@ -19,10 +20,19 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/api/notes", (request, response) => {
+app.get("/api/notes", (req, res) => {
   let file = fs.readFileSync("db/db.json");
   let content = JSON.parse(file);
-  response.json(content);
+  res.json(content);
+});
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  const file = fs.readFileSync("db/db.json");
+  const content = JSON.parse(file);
+  content.push(note);
+  fs.writeFileSync("db/db.json", JSON.stringify(content));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.listen(PORT, () =>
