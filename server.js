a2410325ce7b744a60cecf037679330cc49ce0f4
katochsenthal <katoch.senthal@gmail.com>
2022-05-08 23:05:00 -0400

    delete notes route completed

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = 3001;
 
 const app = express();
 
+const notes = [];
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -29,11 +30,28 @@ app.get("/api/notes", (req, res) => {
 app.post("/api/notes", (req, res) => {
   const file = fs.readFileSync("db/db.json");
   const content = JSON.parse(file);
+  console.log(content);
   const note = { ...req.body, id: content.length + 1 };
 
   content.push(note);
   fs.writeFileSync("db/db.json", JSON.stringify(content));
   res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  let fileContent = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteID = req.params.id;
+  let index = 0;
+  fileContent = fileContent.filter((note) => {
+    console.log("line 47:", note);
+    return note.id != noteID;
+  });
+  for (not of fileContent) {
+    not.id = index.toString();
+    index++;
+  }
+  res.json(fileContent);
+  fs.writeFileSync("./db/db.json", JSON.stringify(fileContent));
 });
 
 app.listen(PORT, () =>
