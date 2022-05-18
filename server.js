2af7b12db11ef5b2eec741405fe234cc6f82494d
katochsenthal <katoch.senthal@gmail.com>
2022-05-17 22:13:50 -0400

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const { get } = require("express/lib/response");
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -54,6 +54,6 @@ app.delete("/api/notes/:id", (req, res) => {
   fs.writeFileSync("./db/db.json", JSON.stringify(fileContent));
 });
 
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+app.listen(PORT, () =>
+  console.log(`App listening at http://0.0.0.0:${PORT} 🚀`)
 );
